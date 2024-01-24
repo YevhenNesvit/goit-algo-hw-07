4d4d2667c0b5931f39ab8d653ce83d149364f120
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-01-24 20:12:29 +0200

    Code refactoring

--- tree_max_value.py
+++ tree_max_value.py
@@ -1,39 +1,6 @@
 import uuid
 
-
-class Node:
-    def __init__(self, key, color="skyblue"):
-        self.left = None
-        self.right = None
-        self.val = key
-        self.color = color
-        self.id = str(uuid.uuid4())
-
-
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    else:
-        if key < root.val:
-            root.left = insert(root.left, key)
-        else:
-            root.right = insert(root.right, key)
-    return root
-
-
-def search(root, key):
-    if root is None or root.val == key:
-        return root
-    if key < root.val:
-        return search(root.left, key)
-    return search(root.right, key)
-
-
-def min_value_node(node):
-    current = node
-    while current.left:
-        current = current.left
-    return current
+from tree import Node, insert, delete
 
 
 def max_value_node(node):
@@ -41,28 +8,6 @@ def max_value_node(node):
     while current.right:
         current = current.right
     return current
-
-
-def delete(root, key):
-    if not root:
-        return root
-
-    if key < root.val:
-        root.left = delete(root.left, key)
-    elif key > root.val:
-        root.right = delete(root.right, key)
-    else:
-        if not root.left:
-            temp = root.right
-            root = None
-            return temp
-        elif not root.right:
-            temp = root.left
-            root = None
-            return temp
-        root.val = min_value_node(root.right).val
-        root.right = delete(root.right, root.val)
-    return root
 
 
 root = Node(11)
--- tree_sum_values.py
+++ tree_sum_values.py
@@ -1,74 +1,12 @@
 import uuid
 
-
-class Node:
-    def __init__(self, key, color="skyblue"):
-        self.left = None
-        self.right = None
-        self.val = key
-        self.color = color
-        self.id = str(uuid.uuid4())
-
-
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    else:
-        if key < root.val:
-            root.left = insert(root.left, key)
-        else:
-            root.right = insert(root.right, key)
-    return root
-
-
-def search(root, key):
-    if root is None or root.val == key:
-        return root
-    if key < root.val:
-        return search(root.left, key)
-    return search(root.right, key)
-
-
-def min_value_node(node):
-    current = node
-    while current.left:
-        current = current.left
-    return current
-
-
-def max_value_node(node):
-    current = node
-    while current.right:
-        current = current.right
-    return current
+from tree import Node, insert
 
 
 def sum_values(root):
     if not root:
         return 0
     return root.val + sum_values(root.left) + sum_values(root.right)
-
-
-def delete(root, key):
-    if not root:
-        return root
-
-    if key < root.val:
-        root.left = delete(root.left, key)
-    elif key > root.val:
-        root.right = delete(root.right, key)
-    else:
-        if not root.left:
-            temp = root.right
-            root = None
-            return temp
-        elif not root.right:
-            temp = root.left
-            root = None
-            return temp
-        root.val = min_value_node(root.right).val
-        root.right = delete(root.right, root.val)
-    return root
 
 
 root = Node(11)
