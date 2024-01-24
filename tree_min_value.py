adc31632dce6de9a084a7f151b558ce318ccf31b
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-01-24 20:07:43 +0200

    initial commit

---
+++ tree_min_value.py
@@ -0,0 +1,73 @@
+import uuid
+
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color
+        self.id = str(uuid.uuid4())
+
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+
+def delete(root, key):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+
+root = Node(11)
+
+root = insert(root, 2)
+root = insert(root, 20)
+root = insert(root, 29)
+root = insert(root, 38)
+root = insert(root, 47)
+root = insert(root, 56)
+
+print(min_value_node(root).val)
+
+delete(root, 2)
+print(min_value_node(root).val)
